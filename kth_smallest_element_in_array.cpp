67d536f2531e618b5ecbc75898c8832a3719cee8
Qazi Maaz Arshad <56196640+QAZIMAAZARSHAD@users.noreply.github.com>
2021-09-17 16:46:20 +0530

    Create kth_smallest_element_in_array.cpp

---
+++ kth_smallest_element_in_array.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    
+    int kthSmallest(int arr[], int l, int r, int k) {
+        
+        sort(arr,arr+r+1);
+        return arr[k-1];
+    }
+};
+
+int main()
+{
+    int test_case;
+    cin>>test_case;
+    while(test_case--)
+    {
+        int number_of_elements;
+        cin>>number_of_elements;
+        int a[number_of_elements];
+        
+        for(int i=0;i<number_of_elements;i++)
+            cin>>a[i];
+            
+        int k;
+        cin>>k;
+        Solution ob;
+        cout<<ob.kthSmallest(a, 0, number_of_elements-1, k)<<endl;
+    }
+    return 0;
+} 
+
+/*
+Input:
+N = 6
+arr[] = 7 10 4 3 20 15
+K = 3
+Output : 7
+Explanation :
+3rd smallest element in the given 
+array is 7.
+
+Input:
+N = 5
+arr[] = 7 10 4 20 15
+K = 4
+Output : 15
+Explanation :
+4th smallest element in the given 
+array is 15.
+*/
