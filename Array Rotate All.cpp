f5ad5c8c7c25550ab1e723ddde6d5a1ea61ad75b
Qazi Maaz Arshad <56196640+QAZIMAAZARSHAD@users.noreply.github.com>
2022-01-24 13:34:39 +0530

    Update Array Rotate All.cpp

--- Array Rotate All.cpp
+++ Array Rotate All.cpp
@@ -18,18 +18,33 @@ void rotate(int a[], int n){
     a[0] = temp;
 }
 
-// Rotate the array to the left (counter-clockwise direction) by D steps
+// Rotate the array to the left (counter-clockwise direction) by d steps
 void rotateArr(int a[], int d, int n){
-        reverse(a,a+d);
-        reverse(a+d,a+n);
-        reverse(a,a+n);
+        reverse(a, a + d);
+        reverse(a + d, a + n);
+        reverse(a, a + n);
 }
 
 // Rotate the vector to the left (counter-clockwise direction) by k steps
 void rotate(vector<int>& a, int k) {
-        reverse(a.begin(),a.begin()+k+1);
-        reverse(a.begin()+k+1,a.end());
-        reverse(a.begin(),a.end());
-    }
+        reverse(a.begin(), a.begin()+k);
+        reverse(a.begin()+k, a.end());
+        reverse(a.begin(), a.end());
+}
 
-// Rotate the array to the right (clockwise direction) by D steps
+// Rotate the array to the right (clockwise direction) by k steps
+void RightRotate(int a[], int n, int k){
+	k = k % n;
+    reverse(a, a + n);
+    reverse(a, a + k);
+    reverse(a + k, a + n);
+}
+
+// Rotate the vector to the right (clockwise direction) by k steps
+void rotate(vector<int>& a, int k) {
+        int n = a.size();
+        k = k % n;
+        reverse(a.begin(), a.end());
+        reverse(a.begin(), a.begin() + k);
+        reverse(a.begin() + k, a.end());
+}
