c6ad639661e1a8922dabb5096285c349cca22b6e
Qazi Maaz Arshad <56196640+QAZIMAAZARSHAD@users.noreply.github.com>
2022-02-18 18:53:21 +0530

    Create SortArrayStringofInteger.cpp

---
+++ SortArrayStringofInteger.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+static bool cmp(string a, string b){
+         return (a.length() < b.length() || (a.length() == b.length() && a < b));
+    }
+
+int main(){
+        vector<string> a = {"2","21","12","1"};
+        int i, n = a.size();
+        for(i=0;i<n;i++){
+            cout<<a[i]<<" ";
+        }
+        cout<<endl;
+        sort(a.begin(), a.end(), cmp);
+        for(i=0;i<n;i++){
+            cout<<a[i]<<" ";
+        }
+        return 0;
+    }
+
+// Input: a = ["2","21","12","1"]
+// Output: ["1","2","12","21"]
